2af95c8916688ed42d2ea5ab95eeb435162a4d87
binglong180 <907555993@qq.com>
2018-01-26 09:36:18 +0800

    使用注解进行事务声明

--- src/com/spring/service/UserServiceImpl.java
+++ src/com/spring/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.spring.dao.user.UserMapper;
 import com.spring.pojo.User;
@@ -36,7 +38,7 @@ public class UserServiceImpl implements UserService {
 	}
 
 
-
+	@Transactional(propagation=Propagation.REQUIRED)
 	public void addUserList(List<User> list) {
 		for(User user:list){
 			addUser(user);
